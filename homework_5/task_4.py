5d8d2fc8550b03f201c7f8c2226ac3c31c65d1ab
dissna <123608388+dissna@users.noreply.github.com>
2023-02-12 10:40:25 +0200

    Add files via upload

--- homework_5/task_4.py
+++ homework_5/task_4.py
@@ -1,8 +1,6 @@
 def my_sum(*elements, start = 0):
-    sum = 0
-    for n in elements:
-        sum += n + start
-    print("Sum: ", sum)
-my_sum(1, 2)
-my_sum(1, 2, 3, 4)
-my_sum(2, 4, 6, 8, 9)
+    return sum(elements) + start
+
+print("Sum: ", my_sum(1, 2))
+print("Sum: ", my_sum(1, 2, 3, 4))
+print("Sum: ", my_sum(2, 4, 6, 8, 9))
