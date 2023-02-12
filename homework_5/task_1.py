148e73667af55c0d7db556c88bd1e04d9c14ad6d
dissna <123608388+dissna@users.noreply.github.com>
2023-02-12 10:14:03 +0200

    Add files via upload

--- homework_5/task_1.py
+++ homework_5/task_1.py
@@ -2,9 +2,10 @@ import math
 def degrees2radians(degree):
     radian = degree / (180/math.pi)
     return radian
-degree = math.cos(60)
-print(degree, "\n""Radians: ", "%.4f" % degrees2radians(degree))
-degree = math.cos(45)
-print(degree, "\n""Radians: ", "%.4f" % degrees2radians(degree))
-degree = math.cos(40)
-print(degree, "\n""Radians: ", "%.4f" % degrees2radians(degree))
+
+degree = 60
+print(degree, "\n""Radians: ", "%.4f" % math.cos(degrees2radians(degree)))
+degree = 45
+print(degree, "\n""Radians: ", "%.4f" % math.cos(degrees2radians(degree)))
+degree = 40
+print(degree, "\n""Radians: ", "%.4f" % math.cos(degrees2radians(degree)))
